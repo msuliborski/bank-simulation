c6b8ae5121dd1747d58e89fd7f4809051dd0a9e7
preczu <ania.preczynska@gmail.com>
2019-01-12 20:15:02 +0100

    quick fix

--- src/banking/Loan.cpp
+++ src/banking/Loan.cpp
@@ -1,49 +1,21 @@
-/**
- * Project Bank
- * @author Michał Suliborski, Anna Preczynska
- * @version 1.0
- */
-
-
 #include "Loan.h"
 
-/**
- * Loan implementation
- */
-
-
-/**
- * @param name
- * @param account
- * @param fee
- * @param months
- * @param day
- */
-void Loan::Loan(string name, shared_ptr<Account> account, double fee, int months, int day) {
+Loan::Loan(string name, shared_ptr<Account> account, double fee, int months, int day) {
 
 }
 
-void Loan::~Loan() {
+Loan::~Loan() {
 
 }
 
-/**
- * @return int
- */
 int Loan::getSettlementDay() {
     return 0;
 }
 
-/**
- * @return int
- */
 int Loan::getMonthsLeft() {
     return 0;
 }
 
-/**
- * @return void
- */
 void Loan::chargeFee() {
     return;
 }
--- src/banking/Loan.h
+++ src/banking/Loan.h
@@ -1,32 +1,16 @@
-/**
- * Project Bank
- * @author Michał Suliborski, Anna Preczynska
- * @version 1.0
- */
-
-
 #ifndef _LOAN_H
 #define _LOAN_H
 
+#include "Account.h"
+
 class Loan {
-public: 
-    
-/**
- * @param name
- * @param account
- * @param fee
- * @param months
- * @param day
- */
-void Loan(string name, shared_ptr<Account> account, double fee, int months, int day);
-    
-void ~Loan();
-    
-int getSettlementDay();
-    
-int getMonthsLeft();
-    
-void chargeFee();
+public:
+    Loan(string name, shared_ptr<Account> account, double fee, int months, int day);
+    ~Loan();
+
+    int getSettlementDay();
+    int getMonthsLeft();
+    void chargeFee();
 private: 
     string name;
     shared_ptr<Account> account;
