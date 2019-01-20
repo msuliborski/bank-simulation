04b8d7de9e4324a5f83edb25206c7ce1f55f8442
preczu <ania.preczynska@gmail.com>
2019-01-20 21:04:28 +0100

    Tests without transfers within accounts

--- test/BankTest.cpp
+++ test/BankTest.cpp
@@ -7,6 +7,9 @@
 #include "../src/system/SimulationLogic.h"
 #include "../src/system/UserInterface.h"
 #include "../src/bank/PersonalAccount.h"
+#include "../src/bank/JuniorAccount.h"
+#include "../src/bank/StudentsAccount.h"
+
 
 using namespace std;
 
@@ -14,14 +17,67 @@ using namespace std;
 BOOST_AUTO_TEST_CASE(test1Test){
 	shared_ptr<Bank> bank(Bank::GetInstance(1000000));
 
-	shared_ptr<Account> account(new PersonalAccount(9999, "test", "test", 500));
+	shared_ptr<Account> personalAccount(new PersonalAccount(9999, "test", "test", 500));
 
-	BOOST_CHECK_EQUAL(account->getPassword(), "test");
-	BOOST_CHECK_EQUAL(account->getAccountType(), "personal");
-	//login
-	//kase
+	BOOST_CHECK_EQUAL(personalAccount->getNumber(), 9999);
+	BOOST_CHECK_EQUAL(personalAccount->getPassword(), "test");
+	BOOST_CHECK_EQUAL(personalAccount->getLogin() , "test");
+	BOOST_CHECK_EQUAL(personalAccount->getAccountType(), "personal");
+	BOOST_CHECK_EQUAL(personalAccount->getBalance(), 500);
+	BOOST_CHECK_EQUAL(personalAccount->getTransferFee(), 5);
+
+	personalAccount->withdraw(120);
+	BOOST_CHECK_EQUAL(personalAccount->getBalance(), 380);
+
+	personalAccount->deposit(30);
+	BOOST_CHECK_EQUAL(personalAccount->getBalance(), 410);
+
+	personalAccount->changeLogin("newLogin");
+	BOOST_CHECK_EQUAL(personalAccount->getLogin() , "newLogin");
+
+	personalAccount->changePassword("newPassword");
+	BOOST_CHECK_EQUAL(personalAccount->getPassword() , "newPassword");
+
+	shared_ptr<Account> juniorAccount(new JuniorAccount(8888, "juniorTest", "juniorTest", 700));
+
+	BOOST_CHECK_EQUAL(juniorAccount->getNumber(), 8888);
+	BOOST_CHECK_EQUAL(juniorAccount->getPassword(), "juniorTest");
+	BOOST_CHECK_EQUAL(juniorAccount->getLogin() , "juniorTest");
+	BOOST_CHECK_EQUAL(juniorAccount->getAccountType(), "junior");
+	BOOST_CHECK_EQUAL(juniorAccount->getBalance(), 700);
+	BOOST_CHECK_EQUAL(juniorAccount->getTransferLimit(), 500);
+
+	BOOST_CHECK_EQUAL(juniorAccount->makeTransfer(9999, "title", 510), false);
+
+	shared_ptr<Account> studentsAccount(new StudentsAccount(7777, "studentTest", "studentTest", 300));
+
+	BOOST_CHECK_EQUAL(studentsAccount->getNumber(), 7777);
+	BOOST_CHECK_EQUAL(studentsAccount->getPassword(), "studentTest");
+	BOOST_CHECK_EQUAL(studentsAccount->getLogin() , "studentTest");
+	BOOST_CHECK_EQUAL(studentsAccount->getAccountType(), "student");
+	BOOST_CHECK_EQUAL(studentsAccount->getBalance(), 300);
+	BOOST_CHECK_EQUAL(studentsAccount->getTransferFee(), 0);
+
+
+	//personalAccount->makeTransfer(8888, "title", 15);
+	//BOOST_CHECK_EQUAL(account->getBalance(), 390);
+//getBlockedBalance
+
 	BOOST_CHECK_LE(bank->getNewAccountNumber(), 9999);
 	BOOST_CHECK_GE(bank->getNewAccountNumber(), 1000);
+
+	bank->addAccount(personalAccount);
+
+	BOOST_CHECK_EQUAL(bank->checkIfLoginAvailable("login"), true);
+	BOOST_CHECK_EQUAL(bank->checkIfLoginAvailable("newLogin"), false);
+
+	bank->deleteAccount(personalAccount->getNumber());
+	BOOST_CHECK_EQUAL(bank->checkIfLoginAvailable("newLogin"), true);
+
+
+
+
+
 	BOOST_CHECK_GE(1, 1);
 	BOOST_CHECK_LT(1, 2);
 	BOOST_CHECK_GT(2, 1);
